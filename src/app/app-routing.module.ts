2832bfee5eb9f94b3950966393c604dfccc5caf4
James Riady <riadyj58@gmail.com>
2021-01-20 11:54:25 +0700

    financial-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FinancialPlannerListComponent } from './components/financial-planner-list/financial-planner-list.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
@@ -7,6 +8,8 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'planner-list', component: FinancialPlannerListComponent },
+  
 ];
 
 @NgModule({
